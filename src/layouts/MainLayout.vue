c7ced1b86b1d5b22586e9d99f9c741f2d3e644d4
Lucas Steinbach <lucaswsb52@gmail.com>
2024-11-17 03:01:28 -0300

    primeiro commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,152 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div class="q-pa-md">
+          <q-btn-dropdown flat color="white" icon="person">
+            <q-list>
+              <q-item clickable v-close-popup  @click="hadleLogout">
+                <q-item-section>
+                  <q-item-label>Logout</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import useAuthUser from 'src/composables/useAuthUser';
+import { useRouter } from 'vue-router';
+import EssentialLink from 'components/EssentialLink.vue'
+import { useQuasar, Dialog } from 'quasar'
+
+const linksList = [
+  {
+    title: 'Docs',
+    caption: 'quasar.dev',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Github',
+    caption: 'github.com/quasarframework',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord Chat Channel',
+    caption: 'chat.quasar.dev',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup() {
+    const router = useRouter();
+    const { logout } = useAuthUser();
+    const $q = useQuasar()
+    const hadleLogout = async () => {
+      $q.dialog({
+        title: 'Logout',
+        message: 'Quer realmente sair ?',
+        cancel: true,
+        persistent: true
+      }).onOk(async () => {
+        await logout();
+        router.replace('/login')
+      })
+
+    }
+
+    return {
+      hadleLogout,
+    }
+  },
+
+  data () {
+    return {
+      linksList,
+      leftDrawerOpen: false
+    }
+  },
+
+  methods: {
+    toggleLeftDrawer () {
+      this.leftDrawerOpen = !this.leftDrawerOpen
+    }
+  }
+})
+</script>
---
+++ src/pages/EmailConfirmation.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+    <p class="text-body1">Obrigado por se cadastrar</p>
+
+    <p class="text-body1">
+      Porfavor confirme seu email para finalizar o cadastro
+    </p>
+
+    <p class="text-body1">
+      Email de cadastro: <strong>{{ $route.query.email }}</strong>
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'emailConfirmation'
+});
+</script>
